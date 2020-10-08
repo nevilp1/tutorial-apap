8ef45d0258730ff6b2fa6de95dcae720f69a6608
Nevil Philia <nevilphiliapramuda@gmail.com>
2020-10-08 23:35:32 +0700

    refactor: tutorial 3 - add package controller

--- traveloke/src/main/java/apap/tutorial/traveloke/controller/HotelController.java
+++ traveloke/src/main/java/apap/tutorial/traveloke/controller/HotelController.java
@@ -1,111 +1,77 @@
 package apap.tutorial.traveloke.controller;
 
 import apap.tutorial.traveloke.model.HotelModel;
+import apap.tutorial.traveloke.model.KamarModel;
 import apap.tutorial.traveloke.service.HotelService;
+import apap.tutorial.traveloke.service.KamarService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @Controller
 public class HotelController {
+    @Qualifier("hotelServiceImpl")
     @Autowired
     private HotelService hotelService;
 
-    // Routing URL yang diinginkan
-    @RequestMapping("/hotel/add")
-    public String addHotel(
-            // Request parameter yang dibawa
-            @RequestParam(value = "idHotel", required = true) String idHotel,
-            @RequestParam(value = "namaHotel", required = true) String namaHotel,
-            @RequestParam(value = "alamat", required = true) String alamat,
-            @RequestParam(value = "noTelepon", required = true) String noTelepon,
+    @Autowired
+    private KamarService kamarService;
+
+    @GetMapping("/")
+    private String home(){
+        return "home";
+    }
+
+    @GetMapping("/hotel/add")
+    public String addHotelFormPage(Model model){
+        model.addAttribute("hotel", new HotelModel());
+        return "form-add-hotel";
+    }
+
+    @PostMapping("/hotel/add")
+    public String addHotelSubmit(
+            @ModelAttribute HotelModel hotel,
             Model model
     ){
-        // Membuat objek HotelModel
-        HotelModel hotel = new HotelModel(idHotel, namaHotel, alamat, noTelepon);
-
-        // Memanggil service addHotel
         hotelService.addHotel(hotel);
-
-        // Add variabel id hotel ke 'idHotel' untuk di render pada thymeleaf
-        model.addAttribute("idHotel", idHotel);
-
-        // Return view template yang digunakan
+        model.addAttribute("idHotel", hotel.getId());
         return "add-hotel";
     }
-    @RequestMapping("/hotel/viewall")
-    public String listHotel(Model model){
 
-        // Mendapatkan semua HotelModel
-        List<HotelModel> listHotel = hotelService.getHotelList();
+    @GetMapping("/hotel/change/{idHotel}")
+    public String changeHotelFormPage(
+            @PathVariable Long idHotel,
+            Model model
 
-        // Add variabel semua HotelModel ke 'listHotel' untuk dirender pada thymeleaf
-        model.addAttribute("listHotel", listHotel);
-
-        // Return view template yang diinginkan
-        return "viewall-hotel";
+    ){
+        HotelModel hotel = hotelService.getHotelByIdHotel(idHotel);
+        model.addAttribute("hotel", hotel);
+        return "form-update-hotel";
     }
-    @RequestMapping("/hotel/view")
-    public String detailHotel(
-            @RequestParam(value = "idHotel") String idHotel,
+
+    @PostMapping("/hotel/change")
+    public String changeHotelFormSubmit(
+            @ModelAttribute HotelModel hotel,
             Model model
     ){
-        // Mendapatkan HotelModel sesuai dengan idHotel
-        HotelModel hotel = hotelService.getHotelByIdHotel(idHotel);
-
-        // Add variabel HotelModel ke 'hotel' untuk di render pada thymeleaf
+        HotelModel hotelUpdated = hotelService.updateHotel(hotel);
         model.addAttribute("hotel", hotel);
-        if(hotel == null) return "hotel-not-found";
+        return "update-hotel";
+    }
 
+    @GetMapping("/hotel/view")
+    public String viewDetailHotel(
+            @RequestParam(value = "idHotel") Long idHotel,
+            Model model
+    ){
+        HotelModel hotel = hotelService.getHotelByIdHotel(idHotel);
+        List<KamarModel> listKamar = kamarService.findAllKamarByIdHotel(idHotel);
+        model.addAttribute("hotel", hotel);
+        model.addAttribute("listKamar", listKamar);
         return "view-hotel";
-    }
-    @GetMapping(value ="/hotel/view/id-hotel/{idHotel}")
-    public String findByID(
-            @PathVariable(value = "idHotel") String idHotel,
-            Model model
-    ){
-        HotelModel hotel = hotelService.getHotelByIdHotel(idHotel);
-        model.addAttribute("hotel", hotel);
-        if(hotel != null)
-            return "view-hotel";
-        return "hotel-not-found";
-    }
-    @GetMapping(value = "/hotel/update/id-hotel/{idHotel}/no-telepon/{nomor}")
-    public String changeNumberByID(
-            @PathVariable(value = "idHotel") String idHotel,
-            @PathVariable(value = "nomor") String nomor,
-            Model model
-    ){
-        HotelModel hotel = hotelService.getHotelByIdHotel(idHotel);
-
-        if(hotel == null) return "hotel-not-found";
-
-        // Update nomor baru pada object hotel
-        hotel.setNoTelepon(nomor);
-
-        // Add variabel HotelModel ke 'hotel' untuk di render pada thymeleaf
-        model.addAttribute("hotel", hotel);
-
-        return "update-nomor";
-    }
-    @GetMapping(value = "/hotel/delete/id-hotel/{idHotel}")
-    public String deleteHotelByID(
-            @PathVariable(value = "idHotel") String idHotel,
-            Model model
-    ){
-        HotelModel hotel = hotelService.getHotelByIdHotel(idHotel);
-
-        if(hotel == null) return "hotel-not-found";
-
-        // Add variabel HotelModel ke 'hotel' untuk di render pada thymeleaf
-        model.addAttribute("hotel", hotel);
-        hotelService.getHotelList().remove(hotel);
-
-        return "delete-hotel";
     }
 }
