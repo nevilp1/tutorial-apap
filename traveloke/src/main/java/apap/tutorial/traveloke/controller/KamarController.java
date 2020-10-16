5b5d44941337153d91af9315478fd88f181e674f
Nevil Philia <nevilphiliapramuda@gmail.com>
2020-10-16 23:27:56 +0700

    refactor: tutorial 4 - update controller

--- traveloke/src/main/java/apap/tutorial/traveloke/controller/KamarController.java
+++ traveloke/src/main/java/apap/tutorial/traveloke/controller/KamarController.java
@@ -9,13 +9,18 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
+
+
 @Controller
 public class KamarController {
+    String statusHalaman;
+
     @Qualifier("hotelServiceImpl")
     @Autowired
     private HotelService hotelService;
@@ -31,10 +36,22 @@ public class KamarController {
         KamarModel kamar = new KamarModel();
         HotelModel hotel = hotelService.getHotelByIdHotel(idHotel);
         kamar.setHotel(hotel);
+
+        model.addAttribute("idHotel", idHotel);
         model.addAttribute("kamar", kamar);
+        statusHalaman = "Form add kamar";
+        model.addAttribute("statusHalaman", statusHalaman);
 
         return "form-add-kamar";
 
+    }
+    @PostMapping(path="/kamar/add/{idHotel}", params = "add_baris")
+    private String addBaris(Model model
+    ){
+        HotelModel hotel = new HotelModel();
+        hotel.getListKamar().add(new KamarModel());
+        model.addAttribute("hotel", hotel.getListKamar());
+        return "form-add-kamar";
     }
 
     @PostMapping("/kamar/add")
@@ -42,16 +59,23 @@ public class KamarController {
         @ModelAttribute KamarModel kamar,
         Model model
     ){
+        statusHalaman = "Add kamar";
+        model.addAttribute("statusHalaman", statusHalaman);
         kamarService.addKamar(kamar);
+
         model.addAttribute("kamar", kamar);
+
         return "add-kamar";
     }
+
     @GetMapping("/kamar/change/{noKamar}")
     public String changeKamarFormPage(
             @PathVariable Long noKamar,
             Model model
 
     ){
+        statusHalaman = "Form update kamar";
+        model.addAttribute("statusHalaman", statusHalaman);
         KamarModel kamar = kamarService.getKamarById(noKamar);
         model.addAttribute("kamar", kamar);
         return "form-update-kamar";
@@ -64,7 +88,23 @@ public class KamarController {
     ){
         KamarModel updateKamar = kamarService.kamarUpdate(kamar);
         model.addAttribute("kamar", kamar);
+        statusHalaman = "Update kamar";
+        model.addAttribute("statusHalaman", statusHalaman);
         return "update-kamar";
+    }
+    @PostMapping(path = "/kamar/delete")
+    public String deleteKamarFormSubmit(
+            @ModelAttribute HotelModel hotel,
+            Model model
+    ){
+        model.addAttribute("kamarCount",hotel.getListKamar().size());
+
+        for(KamarModel kamar : hotel.getListKamar()){
+            kamarService.deleteKamar(kamar);
+        }
+        statusHalaman = "Delete kamar";
+        model.addAttribute("statusHalaman", statusHalaman);
+        return "delete-kamar";
     }
 
     @GetMapping(value = "kamar/delete/{noKamar}")
@@ -73,9 +113,8 @@ public class KamarController {
             Model model
     ){
         // Menghapus Hotel sesuai dengan nomorKamar
-        KamarModel kamar = kamarService.deleteKamar(noKamar);
+        kamarService.deleteKamarId(noKamar);
 
-        model.addAttribute("kamar", kamar);
         return "delete-kamar";
     }
 }
